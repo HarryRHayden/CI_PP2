7d66ab5f718d113a2f37d1b4e4902b45f07c5c2f
Harry Hayden <h_man1992@hotmail.com>
2022-04-06 14:24:04 +0000

    add rules modal to footer section

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+
+// Rules pop-out modal
+var modal = document.getElementById("myRules");
+
+var btn = document.getElementById("how-to-play");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
